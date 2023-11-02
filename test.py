9bed927f4912f4b98ef8d5dd5578dd1d302b9246
andyen11821 <149698782+andyen11821@users.noreply.github.com>
2023-11-02 16:54:53 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(x-calibersizer)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
 
-if 'x-calibersizer' == 'x-calibersizer':
+if __name__ == '__name__':
     app.run(debug=True)
