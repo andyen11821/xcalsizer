9074cc1d0fb2e98a86a32fbe6c1a0236791ba65d
andyen11821 <149698782+andyen11821@users.noreply.github.com>
2023-11-02 11:33:01 -0400

    Update caprates.py

--- caprates.py
+++ caprates.py
@@ -5,5 +5,6 @@ st.write('Hello world')
 cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]  # Add more cities as needed
 city = st.selectbox("Choose a city:", cities)
 
-if st.button("Get Cap Rates"):
+st.button("Get Cap Rates")
 # Code to fetch and display cap rates
+  
