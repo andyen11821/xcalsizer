09f133bb34cc48411497bcba787a00e269ff41f5
andyen11821 <149698782+andyen11821@users.noreply.github.com>
2023-11-02 11:29:24 -0400

    Update caprates.py

--- caprates.py
+++ caprates.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.write('Hello world')
+
+cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]  # Add more cities as needed
+city = st.selectbox("Choose a city:", cities)
+
+if st.button("Get Cap Rates"):
+    # Code to fetch and display cap rates
