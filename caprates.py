b38b8c6f53638458b2b2a5db561f0bd36b7b6ab3
andyen11821 <149698782+andyen11821@users.noreply.github.com>
2023-11-02 12:35:42 -0400

    Update caprates.py

--- caprates.py
+++ caprates.py
@@ -1,42 +1,31 @@
 import streamlit as st
-import pdfkit
-import io
 
 st.write('X-Caliber Capital Automatic Sizer')
 
-projectname = st.text_input("Enter the project name:")
-projectcity = st.text_input("Enter the project city:")
-projectstate = st.text_input("Enter the project state:")
+#projectname = st.text_input("Enter the project name:")
+#projectcity = st.text_input("Enter the project city:")
+#projectstate = st.text_input("Enter the project state:")
 
-ogname = st.text_input("Enter the originator:")
-ogphone = st.text_input("Enter the originator's phone number:")
-ogemail = st.text_input("Enter the originator's email address:")
+#ogname = st.text_input("Enter the originator:")
+#ogphone = st.text_input("Enter the originator's phone number:")
+#ogemail = st.text_input("Enter the originator's email address:")
 
-uwname = st.text_input("Enter the underwriter's name:")
-uwphone = st.text_input("Enter the underwriter's phone number:")
-uwemail = st.text_input("Enter the underwriter's email address:")
+#uwname = st.text_input("Enter the underwriter's name:")
+#uwphone = st.text_input("Enter the underwriter's phone number:")
+#uwemail = st.text_input("Enter the underwriter's email address:")
 
-schedballoon = st.text_input("Enter the Scheduled Balloon (USD):")
-cutoffnoi = st.text_input("Enter the Cutoff NOI (USD):")
-units= st.text_input("Enter the number of units :")
-recentval = st.text_input("Enter the recent valuation (USD):")
+#schedballoon = st.text_input("Enter the Scheduled Balloon (USD):")
+#cutoffnoi = st.text_input("Enter the Cutoff NOI (USD):")
+#units= st.text_input("Enter the number of units :")
+#recentval = st.text_input("Enter the recent valuation (USD):")
 
-loantypes = ["Market", 'Affordable', 'Green']
-loantype = st.selectbox("Choose a type:", loantypes)
+#loantypes = ["Market", 'Affordable', 'Green']
+#loantype = st.selectbox("Choose a type:", loantypes)
 
 cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]  # Add more cities as needed
 city = st.selectbox("Choose a city:", cities)
 
 if st.button("Get Cap Rates"):
-    # Code to fetch and display cap rates
-    # Example content for PDF generation
-    content = f"Commercial Cap Rates for {city}"
-    pdf_bytes = generate_pdf(content)
     
-    # Convert bytes to a BytesIO object
-    pdf_io = io.BytesIO(pdf_bytes)
-    
-    # Download link for PDF
-    st.download_button(label="Download Cap Rates PDF", data=pdf_io, file_name="cap_rates.pdf", mime="application/pdf")
 
   
