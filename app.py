54fd3885d5e6886c1e4f138180b1ce69cda6ebde
andyen11821 <149698782+andyen11821@users.noreply.github.com>
2023-11-03 11:29:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/get_cap_rates', methods=['POST'])
-def get_cap_rates():
-    city = request.form.get('city')
-    return f'Cap rates for {city} are currently unavailable.'
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Get the value from the dropdown
+    selected_city = request.form['city']
+    
+    # Open and read the CSV file
+    output = "No match found."
+    with open('Cap Rates.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row['city'].lower() == selected_city:
+                output = row['info']  # Assume 'info' is the corresponding output column in your CSV
+                break
+    
+    # Return the matching output
+    return f"The information for {selected_city} is: {output}"
 
 if __name__ == '__main__':
     app.run(debug=True)
