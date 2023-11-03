69e0eb2a5f2b253cd78bf1d808955927fcbc07ca
andyen11821 <149698782+andyen11821@users.noreply.github.com>
2023-11-03 18:40:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     # Get the value from the dropdown
-    selected_city = request.form['City-drowdown'].lower()
+    selected_city = request.form['city'].lower()
 
     # URL of the CSV file
     csv_url = 'https://raw.githubusercontent.com/andyen11821/xcalsizer/main/Cap%20Rates.csv'
@@ -32,7 +32,7 @@ def submit():
         reader = csv.DictReader(csvfile)
         for row in reader:
             if row['City'].lower() == selected_city:
-                output = row['Average Cap Rate']  # This should match the header in your CSV for the cap rates
+                output = row['average cap rate']  # This should match the header in your CSV for the cap rates
                 break
     except FileNotFoundError:
         output = "File not found."
