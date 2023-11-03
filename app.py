62c0480c2809c09c859da2b4a354b882b790d6c2
andyen11821 <149698782+andyen11821@users.noreply.github.com>
2023-11-03 11:43:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/', methods=['GET'])
+def form():
+    # A simple form with a dropdown
+    return render_template_string('''
+        <form action="/submit" method="post">
+            <select name="city">
+              <option value="losangeles">Los Angeles</option>
+              <option value="pheonix">Phoenix</option>
+              <option value="chicago">Chicago</option>
+              <option value="houston">Houston</option>
+              <option value="newyork">New York</option>
+            </select>
+            <input type="submit">
+        </form>
+    ''')
+    
 @app.route('/submit', methods=['POST'])
 def submit():
     # Get the value from the dropdown
